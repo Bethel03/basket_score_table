3aae4c7f09311d6527b49b2178a85a5e5d8fd587
Bethel03 <bethelkens9@gmail.com>
2025-08-09 23:19:31 +0200

    Blade logo removed in the navigation,

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Team;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -9,8 +10,9 @@ class HomeController extends Controller
 {
     public function index()
     {
+        $teams = Team::all('team_name');
         if(Auth::check() && Auth::user()->user_type == 'admin'){
-            return view('admin.dashboard');
+            return view('admin.dashboard', compact('teams'));
         }
         elseif(Auth::check() && Auth::user()->user_type == 'user'){
             return view('dashboard');
